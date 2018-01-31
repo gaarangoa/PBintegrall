d589cb16403b2311eacbf456f77378830788e4ec
gaarangoa <gaarangoa@gmail.com>
2018-01-31 16:49:55 -0500

    integron finder

---
+++ run_integron_finder.sh
@@ -0,0 +1,8 @@
+
+files=`ls /data/patric/plasmids/`
+
+for file in $files;
+do
+    echo $file >> log.plasmids
+    integron_finder --local_max --cpu 20 --outdir /data/patric/integrons/plasmid/$file /data/patric/plasmids/$file
+done
