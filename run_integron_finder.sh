d7267c6f3b02f1814d1aa4f705493b15c02a8bf5
gaarangoa <gaarangoa@gmail.com>
2018-01-31 16:59:54 -0500

    fix

--- run_integron_finder.sh
+++ run_integron_finder.sh
@@ -4,5 +4,5 @@ files=`ls /data/patric/plasmids/`
 for file in $files;
 do
     echo $file >> log.plasmids
-    integron_finder --local_max --cpu 20 --outdir /data/patric/integrons/plasmid/$file /data/patric/plasmids/$file
+    integron_finder --local_max --cpu 20 --func_annot --outdir /data/patric/integrons/plasmid/$file /data/patric/plasmids/$file
 done
