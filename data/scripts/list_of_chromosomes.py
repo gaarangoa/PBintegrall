178f862745421b88ce266ba2e773c37cb82a7140
gaarangoa <gaarangoa@gmail.com>
2018-05-25 16:30:04 -0400

    fix

--- data/scripts/list_of_chromosomes.py
+++ data/scripts/list_of_chromosomes.py
@@ -1,5 +1,5 @@
 # This scripts traverses a file from the PATRIC database and splits the files into chromosome sequences and plasmids.
-# /data/patric/genomes/1133852.3 
+# /data/patric/genomes/1133852.3
 
 import sys
 
@@ -16,14 +16,16 @@ pfile = open(diro+'/plasmids.txt', 'w')
 cfile = open(diro+'/chromosomes.txt', 'w')
 
 for ix,record in enumerate(SeqIO.parse(diri+"/"+fi+"/"+fi+".fna", "fasta")):
-    gid = record.description 
+    gid = record.description
     seq = record.seq
 
-    if "plasmid" in gid: 
+    if "plasmid" in gid:
         gtype="plasmids"
         pfile.write(gtype+'/'+fi+"_"+str(ix)+'.fasta\n')
+        print(gtype, fi, ix)
     else:
         gtype="chromosomes"
         cfile.write(gtype+'/'+fi+"_"+str(ix)+'.fasta\n')
+        print(gtype, fi, ix)
 
 
