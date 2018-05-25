88828efe2a1dbd2be378c3cef24dd509b5f0c433
gaarangoa <gaarangoa@gmail.com>
2018-05-25 16:31:33 -0400

    fix

--- data/scripts/split-all-genomes.sh
+++ data/scripts/split-all-genomes.sh
@@ -1,7 +1,7 @@
 # list all genomes:
-# $1 genomes directory 
+# $1 genomes directory
 # $2 input directory
-# $3 output directory 
+# $3 output directory
 
 dix=$1
 dox=$2
@@ -9,8 +9,8 @@ dox=$2
 list=`ls $dix` # input the directory where all the genomes are
 
 for g in $list;
-do 
-    echo $g
+do
+    # echo $g
     # python substract-chromosomes-plasmids.py $g $dix $dox
     python list_of_chromosomes.py $g $dix /data/patric/integrons/
 done
